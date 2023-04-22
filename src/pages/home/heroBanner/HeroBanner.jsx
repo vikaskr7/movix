6468bd0e4a660b2331eaae141f5016f3cf3ca2a2
Vikas Kumar <113836744+vikaskr7@users.noreply.github.com>
2023-04-22 10:57:31 +0530

    Update HeroBanner.jsx

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -42,7 +42,7 @@ const HeroBanner = () => {
           <div className="heroBannerContent">
             <span className="title">Welcome</span>
             <span className="subTitle">
-              Millions of movies, TV shows and people to discover. Explore now.
+              Explore Millions of movies, TV shows and people to discover. Explore now.
             </span>
             <div className="searchInput">
               <input
